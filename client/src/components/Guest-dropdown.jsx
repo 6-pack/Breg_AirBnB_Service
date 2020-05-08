f585a237a1b6663dd05fba87e8b037bb509f0581
Breg Bromley <bregbromley@gmail.com>
2020-05-07 18:21:55 -0600

    added functionality to guest dropdown buttons

--- client/src/components/Guest-dropdown.jsx
+++ client/src/components/Guest-dropdown.jsx
@@ -14,36 +14,40 @@ class Dropdown extends React.Component {
       return (
         <div className="dropdown">
           <div className="margin1">
-            <div className="grid2">
-              <span className="margin">Adults</span>
-              <span>
-                <button className="asde">—</button>
-                <span className="margin">0</span>
-                <button className="asde">＋</button>
-              </span>
-            </div>
-            <div className="grid2">
-              <span className="margin">Children</span>
-              <span>
-                <button className="asde">—</button>
-                <span className="margin">0</span>
-                <button className="asde">＋</button>
-              </span>
-            </div>
-            <div className="grid2">
-              <span className="margin">Infants</span>
-              <span>
-                <button className="asde">—</button>
-                <span className="margin">0</span>
-                <button className="asde">＋</button>
-              </span>
-            </div>
+            <Item name="Adults" click={this.props.click} nums = {this.props.nums} />
+            <Item name="Children" click={this.props.click} nums = {this.props.nums} />
+            <Item name="Infants" click={this.props.click} nums = {this.props.nums} />
           </div>
+          <div className="margin wide">6 guests maximum. Infants don’t count toward the number of guests.</div>
+          <div className="right margin" onClick={this.props.close}>Close</div>
         </div>
       );
     }
     return null;
   }
 }
+
+const Item = function ({ name, click, nums}) {
+  let text = '';
+  if (name === "Children") {
+    text = 'Ages 2–12';
+  } else if (name === "Infants") {
+    text = 'Under 2';
+  }
+
+  return (
+    <div className="grid2 margin">
+      <div className="grid3 centfl">
+        <div className="centfl">{name}</div>
+        <div className=" normal">{text}</div>
+      </div>
+      <div className="autom">
+        <button className="asde" onClick={()=>{click('-', name)}}>—</button>
+        <span className="margin">{nums[name]}</span>
+        <button className="asde" onClick={()=>{click('+', name)}}>＋</button>
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
--- client/src/components/Guests.jsx
+++ client/src/components/Guests.jsx
@@ -9,6 +9,11 @@ class Guests extends React.Component {
 
     this.state = {
       open: false,
+      Adults: 1,
+      Children: 0,
+      Infants: 0,
+      max: 4,
+      guests: 1
     };
   }
 
@@ -23,20 +28,62 @@ class Guests extends React.Component {
     console.log(that.state);
   }
 
+  guestHandler(action, ele) {
+    const that = this;
+    console.log(action, ele);
+    if (ele === 'Adults') {
+      if (action === '-') {
+        if (that.state.Adults > 1) {
+          const newVal = that.state.Adults - 1;
+          const newGuest = that.state.guests - 1;
+          that.setState({ Adults: newVal });
+          that.setState({ guests: newGuest });
+        }
+      } else if (that.state.guests < that.state.max) {
+        const newVal = that.state.Adults + 1;
+        const newGuest = that.state.guests + 1;
+        that.setState({ Adults: newVal });
+        that.setState({ guests: newGuest });
+      }
+    }
+    if (ele === 'Children') {
+      if (action === '-') {
+        if (that.state.Children > 1) {
+          const newVal = that.state.Children - 1;
+          const newGuest = that.state.guests - 1;
+          that.setState({ Children: newVal });
+          that.setState({ guests: newGuest });
+        }
+      } else if (that.state.guests < that.state.max) {
+        const newVal = that.state.Children + 1;
+        const newGuest = that.state.guests + 1;
+        that.setState({ Children: newVal });
+        that.setState({ guests: newGuest });
+      }
+    }
+    if (ele === 'Infants') {
+      if (action === '-') {
+        if (that.state.Infants > 0) {
+          const newVal = that.state.Infants - 1;
+          that.setState({ Infants: newVal });
+        }
+      } else if (that.state.Infants < 5) {
+        const newVal = that.state.Infants + 1;
+        that.setState({ Infants: newVal });
+      }
+    }
+    console.log(that.state);
+  }
+
   render() {
     return (
-      <div onClick={this.clickHandler.bind(this)}>
+      <div>
         <div className="label">Guests</div>
-        <div className="dates box grid guests">
+        <div className="dates box grid guests" onClick={this.clickHandler.bind(this)}>
           3 guests
         </div>
-        <Dropdown open={this.state.open} />
+        <Dropdown open={this.state.open} click={this.guestHandler.bind(this)} nums={this.state} close={this.clickHandler.bind(this)} />
         <br className="clear" />
-        <select id="Guests">
-          <option value="Adults">Adults</option>
-          <option value="Infants">Children</option>
-          <option value="Infants">Infants</option>
-        </select>
       </div>
     );
   }
